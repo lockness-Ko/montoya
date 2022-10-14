b8bd70b425427d3b63f3e751673931f19c5c9569
stackotter <stackotter@stackotter.dev>
2022-10-14 14:49:56 +1000

    Create XSS detection helper

--- browser_extension/harness.js
+++ browser_extension/harness.js
@@ -1,3 +1,74 @@
+/* Constants */
+const xssInjectionPayload = "aa<bbcc>dd";
+
+/* Util */
+const extractMatchingExcerpts = (text, searchString) => {
+  var excerpts = [];
+  while (true) {
+    var position = text.search(searchString);
+    if (position == -1) {
+      break;
+    }
+
+    let max = text.length - 1;
+    var excerptStart = Math.max(position - 10, 0);
+    var excerptEnd = Math.min(position + 10 + searchString.length, max);
+    var excerpt = text.slice(excerptStart, excerptEnd).trim();
+    if (excerptStart != 0) {
+      excerpt = "... " + excerpt;
+    }
+    if (excerptEnd != max) {
+      excerpt += " ...";
+    }
+
+    excerpts.push(excerpt);
+
+    text = text.replace(xssInjectionPayload, "_");
+  }
+
+  return excerpts;
+}
+
+/* Keyboard shortcuts */
+const shortcuts = {
+  "KeyP": () => {
+    var input = document.activeElement;
+    if (input["value"] !== undefined) {
+      console.log("typing");
+      input.value += xssInjectionPayload;
+    } else {
+      console.log("Not input");
+    }
+  },
+  "KeyF": () => {
+    console.log("Searching for successful XSS attempts...");
+
+    var html = document.documentElement.outerHTML;
+
+    var possibleXSSExcerpts = extractMatchingExcerpts(html, xssInjectionPayload);
+    possibleXSSExcerpts.forEach((excerpt) => {
+      console.info(`Found possible XSS!
+  ${excerpt}`);
+    });
+
+    if (possibleXSSExcerpts.length == 0) {
+      console.log("No successful injections found");
+    }
+
+    console.log("Search completed");
+  }
+}
+
+document.addEventListener('keydown', (e) => {
+  Object.entries(shortcuts).forEach(entry => {
+    const [key, action] = entry;
+    if (e.code === key && e.altKey && e.ctrlKey) {
+      action();
+    }
+  });
+});
+
+/* Banner */
 console.log(`
  __  __  ______  ____     __  __   ______   __  __  ____        __  __  ____    __       ____    ____    ____
 /\\ \\/\\ \\/\\  _  \\/\\  _\`\\  /\\ \\/\\ \\ /\\__  _\\ /\\ \\/\\ \\/\\  _\`\\     /\\ \\/\\ \\/\\  _\`\\ /\\ \\     /\\  _\`\\ /\\  _\`\\ /\\  _\`\\
--- test_site/app.py
+++ test_site/app.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+  return render_template("index.html")
+
+@app.route("/xssable")
+def xssable():
+  return "You entered '" + request.args["input"] + "'<br/><a href='/'>Go back</a>"
 
 app.run("0.0.0.0", port=1337)
