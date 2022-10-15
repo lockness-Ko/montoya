1f05164d19bc8f1ac328e2233edb67cc494bf686
stackotter <stackotter@stackotter.dev>
2022-10-15 15:55:31 +1000

    Fix extension popup iframe error handling

--- browser_extension/popup/iframe_handler.js
+++ browser_extension/popup/iframe_handler.js
@@ -1,9 +1,15 @@
-var target = document.getElementById("target");
-var frame = document.getElementById("frame");
+document.onreadystatechange = function () {
+  console.log("change");
+  if (document.readyState == "interactive") {
+    console.log("load");
+    var target = document.getElementById("target");
+    var frame = document.getElementById("frame");
 
-window.onload = () => {
-  frame.addEventListener("error", (e) => {
-    target.innerHTML = "";
-    target.textContent = "ERROR: Failed to load companion, are you sure that it's running?";
-  })
+    console.log("requesting site");
+    fetch(frame.src)
+      .catch(() => {
+        target.innerHTML = `ERROR: Failed to load companion, are you sure that it's running?
+<style>#target { padding: 2em; box-sizing: border-box; }</style>`;
+      });
+  }
 };
